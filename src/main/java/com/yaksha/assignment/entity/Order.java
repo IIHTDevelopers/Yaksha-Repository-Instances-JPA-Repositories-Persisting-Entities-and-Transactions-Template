b47fbfbc13e1b28a4240babdfdb383850976a0c5
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-06-04 12:09:48 +0530

    Update Order.java

--- src/main/java/com/yaksha/assignment/entity/Order.java
+++ src/main/java/com/yaksha/assignment/entity/Order.java
@@ -1,15 +1,7 @@
 package com.yaksha.assignment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity(name = "orders")
 public class Order {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private Long productId;
