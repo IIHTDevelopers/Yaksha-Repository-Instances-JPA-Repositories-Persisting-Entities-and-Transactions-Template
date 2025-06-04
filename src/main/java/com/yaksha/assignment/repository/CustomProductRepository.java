9bbda56014868407da6efdf71c566d3a217f09d3
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-06-04 12:11:21 +0530

    Update CustomProductRepository.java

--- src/main/java/com/yaksha/assignment/repository/CustomProductRepository.java
+++ src/main/java/com/yaksha/assignment/repository/CustomProductRepository.java
@@ -7,17 +7,13 @@ import com.yaksha.assignment.entity.Product;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
-@Repository
 public class CustomProductRepository {
 
-	@PersistenceContext
 	private EntityManager entityManager;
 
 	public void updateProductStock(Long productId, Integer quantitySold) {
-		Product product = entityManager.find(Product.class, productId);
-		if (product != null) {
+		
 			// Decrease stock based on quantity sold
-			product.setStock(product.getStock() - quantitySold);
-		}
+		
 	}
 }
