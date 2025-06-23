6e0fdb07e4ae9594f99559e575bad234443d3173
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-06-23 17:35:58 +0530

    Update ProductService.java

--- src/main/java/com/yaksha/assignment/service/ProductService.java
+++ src/main/java/com/yaksha/assignment/service/ProductService.java
@@ -1,10 +1,24 @@
 package com.yaksha.assignment.service;
 
+import java.util.List;
+import com.yaksha.assignment.entity.Product;
+
 public class ProductService {
 
 	// Get all products
+	public List<Product> getAllProducts() {
+		// write your logic here
+		return null;
+	}
 
 	// Create or update a product
+	public Product saveProduct(Product product) {
+		// write your logic here
+		return null;
+	}
 
 	// Delete a product by ID
+	public void deleteProduct(Long id) {
+		// write your logic here
+	}
 }
